0833c9050b8b240edaa9b182be99ee7e0367b343
jackyoh <yangj26952@gmail.com>
2016-09-25 16:24:29 +0800

    Add webduino example

--- wbduino-simple-example/wbduino-simple-example.ino
+++ wbduino-simple-example/wbduino-simple-example.ino
@@ -3,6 +3,9 @@
 #include "Ethernet.h"
 #include "WebServer.h"
 #include "Streaming.h"
+#include <dht.h>
+#define DHT11_PIN 2
+dht DHT;
 
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 IPAddress ip(192, 168, 0, 177);
@@ -21,11 +24,49 @@ void defaultCmd(WebServer &server,WebServer::ConnectionType type, char *url_tail
   }
   
 }
+void dhtCmd(WebServer &server, WebServer::ConnectionType type, char *url_tail,bool tail_complete){
+  P(dntHead) =
+    "<!DOCTYPE HTML>"
+    "<html>"
+       "<head><title>test</title></head>"
+       "<body>";
+       
+  P(dntEnd) =
+       "<a href=\"http://192.168.0.177\">GO HOME</a>"
+       "</body>"
+       "</html>";
+
+  server.httpSuccess();
+  if(type != WebServer::HEAD){
+      server.printP(dntHead);
+  }
+
+  int chk = DHT.read11(DHT11_PIN);
+  switch(chk){
+    case DHTLIB_OK:
+        Serial.print("OK, \t");
+        break;
+    case DHTLIB_ERROR_CHECKSUM:
+        Serial.print("Checksum error,\t");
+        break;
+    case DHTLIB_ERROR_TIMEOUT:
+        Serial.print("Time out error,\t");
+        break;
+    default:
+        Serial.print("Unknown error, \t");
+        break;
+  }
+
+  server<<"<span style='color:red;'>humidity</span>"<<DHT.humidity<<"<br/>";
+  server<<"<span style='color:red;'>temperature</span>"<<DHT.temperature<<"<br/>";
+  server.printP(dntEnd);
+}
 
 void setup() {
   Ethernet.begin(mac, ip);
   webserver.begin();
   webserver.setDefaultCommand(&defaultCmd);
+  webserver.addCommand("dht", &dhtCmd);
 }
 
 void loop() {
